851941d699b7c99570add7a3e7e0486691e6c8a5
tyagivb <45883027+tyagivb@users.noreply.github.com>
2018-12-18 23:46:34 +0600

    Update Html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AssetFormComponent } from './asset-form/asset-form.component';
 
-const routes: Routes = [
-  { path: '', loadChildren: './layout/layout.module#LayoutModule' },
-  { path: 'login', loadChildren: './login-form/login.module#LoginModule' },
-  { path: '**', redirectTo: 'not-found' }
+const routes: Routes = [  
+  // {path: 'user', component : UserListComponent},
+  // {path: 'login', component : LoginComponent},
+  // {path: 'list', component : ListEmployeesComponent, resolve : { empList :  EmployeeListResolverService}},
+  // {path : 'edit/:id' , component : CreateEmployeeComponent, canDeactivate: [CreateEmployeeCanDeactivateGaurdService]},
+  // {path: 'employees/:id', component : EmployeeDetailComponent,
+  //  canActivate : [EmployeeGuardService]},
+  // {path : '', redirectTo: '/login', pathMatch: 'full'},
+  // {path : 'notfound' , component: PageNotFoundComponent}
+   { path: '', loadChildren: './layout/layout.module#LayoutModule' },
+   { path: 'login', loadChildren: './login-form/login.module#LoginModule' },
+   {path: 'AddAsset', component : AssetFormComponent},
+   { path: '**', redirectTo: 'not-found' }
 ];
 
 
